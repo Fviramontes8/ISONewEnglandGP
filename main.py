914073d08967fb977b45b42bc0679ab22ab9f97d
frankie <fviramontes8@unm.edu>
2022-01-13 10:23:06 -0700

    Add feature plot function
    Added a function called plot_ISO_features which will plot all features
    taken from the data source

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 import pandas as pd
 import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+import torch
+import gpytorch
 
 import PlotUtils as pu
+import GPyTorchUtils as gptu
 
 def read_xls(xls_filename, sheet):
     return pd.read_excel(xls_filename, sheet_name=sheet)
 
+def plot_ISO_features(feature_data, feature_descriptions, feature_units):
+    for data, description, units in zip(
+        feature_data, 
+        feature_descriptions, 
+        feature_units
+    ): 
+        pu.general_plot(data, description, "Time (Hours)", units)
+    
+
 # Global variables are bad, m'kay?
 def main():
-    NH_data = read_xls("csv_data/2011_smd_hourly.xls", "NH")
+    NH_data = read_xls("xls_data/2011_smd_hourly.xls", "NH")
     feature_data = np.array(
         [
             NH_data["DEMAND"], 
@@ -26,14 +41,13 @@ def main():
         "Temperature (Fahrenheit)",
         "Temperature (Fahrenheit)"
     ]
-    print(f"Feature shape: {feature_data.shape}")
 
-    for f_data, f_descript, f_units in zip(
-        feature_data, 
-        feature_description, 
-        feature_units
-    ): 
-        pu.general_plot(f_data, f_descript, "Time (Hours)", f_units)
+    plot_ISO_features(feature_data, feature_description, feature_units)
+
+    train_x, train_y, test_x, test_y = torch_train_test_split(
+        feature_data[0], 
+        0.5
+    )
     
 
 if __name__ == "__main__":

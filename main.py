6dfacdd0a3185218e5c5c19ea6360124dae7b15d
frankie <fviramontes8@unm.edu>
2022-02-04 22:46:30 -0700

    Update main.py
    Updated main.py for a successful experiment where the a GP takes a days
    worth of data (24) hours to predict the 37th hour (1 pm) of the next
    day.
    
    Updated TorchTrain in GPyTorchUtils.py to return loss to view loss over
    time to confirm training is reducing error.

--- main.py
+++ main.py
@@ -42,28 +42,6 @@ def torch_one_hour_data_split(data):
     test_y = torch.Tensor([data[begin+36]])
     return train_x, train_y, test_x, test_y
 
-def torch_one_day_data_split(data):
-    assert(len(data)>35)
-    # Inclusive beginning
-    begin=0
-    # Exclusive end
-    end=24
-
-    train_x = torch.Tensor(data[begin:end]).view(1, 24)
-    train_y = torch.Tensor([data[end:end+24]])
-
-    begin = end
-    end += 24
-    while(end < (len(data) - 48)):
-        train_x = torch.vstack([train_x, torch.Tensor(data[begin:end])])
-        train_y = torch.cat([train_y, torch.Tensor([data[end:end+24]])])
-        begin = end
-        end += 24
-
-    test_x = torch.Tensor([data[begin:end]])
-    test_y = torch.Tensor([data[end:end+24]])
-    return train_x, train_y, test_x, test_y
-
 def one_hour_prediction(input_data):
     train_x, train_y, test_x, test_y = torch_one_hour_data_split(input_data)
     test_x = test_x.view(1, -1)
@@ -87,31 +65,6 @@ def one_hour_prediction(input_data):
     print(f"{test_y.item()}")
     print(f"{pred.mean.numpy()}")
 
-class OneDayLinearGP():
-    def __init__(self, train_x, train_y, likelihood):
-        self.train_x = train_x
-        self.train_y = train_y
-        self.likelihood = likelihood
-        self.models = [
-            gptu.LinearGPModel(train_x, i, likelihood) for i in train_y
-        ]
-
-    def train(self, optimizers, iterations):
-        for i, j, k in zip(self.train_y, self.models, optimizers):
-            gptu.TorchTrain(
-                self.train_x, 
-                i, 
-                j, 
-                self.likelihood, 
-                k, 
-                iterations
-            )
-    def test(self, test_x):
-        self.pred = [
-            gptu.TorchTest(test_x, i, self.likelihood).mean.numpy() 
-            for i in self.models
-        ]
-
 # Global variables are bad, m'kay?
 def main():
     NE_data = read_xls("xls_data/2011_smd_hourly.xls", "ISONE CA")
@@ -132,45 +85,7 @@ def main():
     '''
 
     print(f"Data length: {len(feature_data)}")
-    #one_hour_prediction(feature_data)
-
-    train_x, train_y, test_x, test_y = torch_one_day_data_split(feature_data)
-    #print(f"Train x: {train_x.shape}\n")
-    #print(f"Train y: {train_y.shape}\n")
-    #print(f"Test x: {test_x.shape}")
-    #print(f"Test y: {test_y.shape}")
-
-    print(f"Train x: {train_x}\n")
-    print(f"Train y: {train_y}\n")
-
-    '''
-    likelihood = gpytorch.likelihoods.GaussianLikelihood()
-    kernel = gpytorch.kernels.ScaleKernel(
-        gpytorch.kernels.LinearKernel() + gpytorch.kernels.RBFKernel()
-    )
-    gp_model = OneDayLinearGP(train_x, train_y, likelihood)
-    optimizers = [
-        torch.optim.Adam(
-            [
-                {"params" : i.parameters(),}
-            ],
-            lr = 0.1
-        )
-        for i in gp_model.models
-    ]
-    gp_model.train(optimizers, 10)
-    gp_model.test(test_x)
-    print(f"{gp_model.pred}")
-    '''
-    
-    #gptu.TorchTrain(train_x, train_y, gp_model, likelihood, optimizer, 10)
-    #pred = gptu.TorchTest(test_x, gp_model, likelihood)
-    #print(f"{test_y.numpy()}")
-    #print(f"{pred.mean.numpy()}")
-
-    #plt.plot(train_x, train_y)
-    #pu.PlotGPPred(test_x, test_y, test_x, pred)
-    #pu.PlotGPPred(train_x, train_y, train_x, pred)
+    one_hour_prediction(feature_data)
 
 if __name__ == "__main__":
     main()

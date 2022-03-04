51c071821db631e99c38d35686637cbf8700f3ae
frankie <fviramontes8@unm.edu>
2022-03-03 22:43:40 -0700

    Update main.py
    Added visualization of one day prediction, not only does it compute the
    MAPE, but also shows mean absolute error and the r-squared value.
    Details on the implementation can be found in GPyTorchUtils.py in the
    OneDayLinearGP class.

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ import PlotUtils as pu
 import GPyTorchUtils as gptu
 import SignalProcessor as sp
 
+from sklearn.metrics import mean_absolute_error as mae
+from sklearn.metrics import r2_score
+
 def plot_ISO_features(feature_data, feature_descriptions, feature_units):
     for data, description, units in zip(
         feature_data, 
@@ -63,7 +66,6 @@ def one_hour_prediction(input_data):
     #print(f"Test_x shape: {test_x.shape}")
     #print(f"Test_y shape: {test_y.shape}\n")
     
-    '''
     likelihood = gpytorch.likelihoods.GaussianLikelihood()
     gp_model = gptu.LinearGPModel(train_x, train_y, likelihood)
     optimizer = torch.optim.Adam(
@@ -73,38 +75,19 @@ def one_hour_prediction(input_data):
         lr = 0.1
     )
     
-    train_loss = gptu.TorchTrain(train_x, train_y, gp_model, likelihood, optimizer, 50, True)
+    train_loss = gptu.TorchTrain(
+        train_x, 
+        train_y, 
+        gp_model, 
+        likelihood, 
+        optimizer, 
+        50, 
+        True
+    )
     pu.general_plot(train_loss, "Loss over time")
     pred = gptu.TorchTest(test_x, gp_model, likelihood)
     print(f"{test_y.item()}")
     print(f"{pred.mean.numpy()}")
-    '''
-
-class OneDayLinearGP():
-    def __init__(self, train_x, train_y_mat, likelihood):
-        self.train_x = train_x
-        self.train_y = train_y_mat
-        self.likelihood = likelihood
-        self.models = [
-            gptu.LinearGPModel(train_x, i, likelihood) for i in train_y_mat
-        ]
-
-    def train(self, optimizers, iterations):
-        for i, j, k in zip(self.train_y, self.models, optimizers):
-            gptu.TorchTrain(
-                self.train_x, 
-                i, 
-                j, 
-                self.likelihood, 
-                k, 
-                iterations
-            )
-            print("")
-    def test(self, test_x):
-        self.pred = [
-            gptu.TorchTest(test_x, i, self.likelihood).mean.numpy() 
-            for i in self.models
-        ]
 
 # Global variables are bad, m'kay?
 def main():
@@ -126,7 +109,7 @@ def main():
     '''
 
     print(f"Data length: {len(feature_data)}")
-    one_hour_prediction(feature_data)
+    #one_hour_prediction(feature_data)
 
     train_x, train_y, test_x, test_y = torch_one_hour_data_split(feature_data)
     train2_y, test2_y = torch_one_hour_target_split(feature_data, 11)
@@ -135,20 +118,12 @@ def main():
         train2_y = torch.vstack([train2_y, tr_y])
         test2_y = torch.vstack([test2_y, te_y])
 
-    #train2_y = torch.transpose(train2_y, 1, 0)
     test2_y = torch.squeeze(test2_y)
     test_x = torch.unsqueeze(test_x, 0)
 
-    #print(f"Train x: {train_x.shape}")
-    #print(f"Train y: {train_y.shape}\n")
-    #print(f"Test x: {test_x.shape}")
-    #print(f"Test y: {test_y.shape}")
-    #print(f"train2_y: {train2_y.shape}")
-    #print(f"test2_y: {test2_y.shape}\n")
-
 
     likelihood = gpytorch.likelihoods.GaussianLikelihood()
-    gp_model = OneDayLinearGP(train_x, train2_y, likelihood)
+    gp_model = gptu.OneDayLinearGP(train_x, train2_y, likelihood)
 
     optimizers = [
         torch.optim.Adam(
@@ -161,23 +136,38 @@ def main():
     ]
     gp_model.train(optimizers, 10)
     gp_model.test(test_x)
-    for i in range(24):
-        print(f"{gp_model.pred[i][0]:.1f} {test2_y[i]}")
-    local_pred = [i for i in gp_model.pred]
 
-    test_x_plot = [i for i in range(24)]
-    plt.plot(test_x_plot, test2_y, label="Actual")
-    plt.plot(test_x_plot, local_pred, label="Predicted")
-    plt.title("Linear Gaussian Process prediction one day ahead")
-    plt.xlabel("Time (hours)")
-    plt.ylabel("Load demand (MW)")
-    plt.legend()
-    plt.show()
+    local_pred = [i.mean.numpy() for i in gp_model.pred]
 
-    print(f"MAPE: {sp.mape_test(np.array(test2_y), np.array(local_pred)):.3f}")
-    #plt.plot(train_x, train_y)
-    #pu.PlotGPPred(test_x, test2_y, test_x, local_pred)
-    #pu.PlotGPPred(train_x, train_y, train_x, pred)
+    print(f"\nMAPE: {sp.mape_test(np.array(test2_y), np.array(local_pred)):.3f}")
+    print(f"Mean absolute error: {mae(test2_y, local_pred):.3f}")
+    print(f"r^2 value: {r2_score(test2_y, local_pred):.3f}")
+
+    confi_sigma1 = gptu.confirm_confidence_region(
+        local_pred, 
+        test2_y, 
+        gp_model.upper_sigma, 
+        gp_model.lower_sigma
+    ) 
+    print(f"{confi_sigma1}% is contained in a confidence interval", end='')
+    print(f"of 1 standard deviation")
+
+    confi_sigma2 = gptu.confirm_confidence_region(
+        local_pred, 
+        test2_y, 
+        gp_model.upper_two_sigma, 
+        gp_model.lower_two_sigma
+    )
+    print(f"{confi_sigma2}% is contained in a confidence interval", end='')
+    print(f"of 2 standard deviations")
+
+    pred_title = "Linear Gaussian Process prediction one day ahead"
+    xlabel = "Time (hours)"
+    ylabel = "Load demand (MW)"
+
+    gp_model.plot_pred(test2_y, pred_title, xlabel, ylabel)
+
+
 
 if __name__ == "__main__":
     main()

43b1682f75905fa0039aa6a77bdce6cfa0dd06b5
frankie <fviramontes8@unm.edu>
2022-03-30 14:17:17 -0600

    Update main
    Updated main by removing functions used to parse the input data
    (torch_one_hour_data_split and torch_one_hour_target_split) and moving
    it to GPyTorchUtils. Updated function calls so that they access the
    GPyTorchUtils module.

--- main.py
+++ main.py
@@ -12,59 +12,9 @@ import SignalProcessor as sp
 from sklearn.metrics import mean_absolute_error as mae
 from sklearn.metrics import r2_score
 
-def plot_ISO_features(feature_data, feature_descriptions, feature_units):
-    for data, description, units in zip(
-        feature_data, 
-        feature_descriptions, 
-        feature_units
-    ): 
-        pu.general_plot(data, description, "Time (Hours)", units)
-    
-def torch_one_hour_data_split(data, offset=11):
-    assert(len(data)>48)
-    # Inclusive beginning 
-    begin=0
-    # Exclusive end
-    end=24
-    offset += 24
-
-    train_x = torch.Tensor(data[begin:end]).view(1, 24)
-    train_y = torch.Tensor([data[offset]])
-
-    begin = end
-    end += 24
-    while(end < (len(data) - 48)):
-        train_x = torch.vstack([train_x, torch.Tensor(data[begin:end])])
-        train_y = torch.cat([train_y, torch.Tensor([data[begin+offset]])])
-        begin = end
-        end += 24
-
-    test_x = torch.Tensor(data[begin:end])
-    test_y = torch.Tensor([data[begin+offset]])
-    return train_x, train_y, test_x, test_y
-
-def torch_one_hour_target_split(data, offset=0):
-    begin=0
-    end=24
-    offset += 24
-    train_y = torch.Tensor([data[offset]])
-
-    begin = end
-    end += 24
-    while(end < (len(data) - 48)):
-        train_y = torch.cat([train_y, torch.Tensor([data[begin+offset]])])
-        begin = end
-        end += 24
-    test_y = torch.Tensor([data[begin+offset]])
-    return train_y, test_y
-
 def one_hour_prediction(input_data):
-    train_x, train_y, test_x, test_y = torch_one_hour_data_split(input_data)
+    train_x, train_y, test_x, test_y = gptu.torch_one_hour_data_split(input_data)
     test_x = test_x.view(1, -1)
-    #print(f"Train_x shape: {train_x.shape}")
-    #print(f"Train_y shape: {train_y.shape}\n")
-    #print(f"Test_x shape: {test_x.shape}")
-    #print(f"Test_y shape: {test_y.shape}\n")
     
     likelihood = gpytorch.likelihoods.GaussianLikelihood()
     gp_model = gptu.LinearGPModel(train_x, train_y, likelihood)
@@ -89,7 +39,6 @@ def one_hour_prediction(input_data):
     print(f"{test_y.item()}")
     print(f"{pred.mean.numpy()}")
 
-# Global variables are bad, m'kay?
 def main():
     NE_data = np.load("data/ISONE_CA_DEMAND.npy")
     data_cutoff = (24 * 12) #192
@@ -111,19 +60,23 @@ def main():
     print(f"Data length: {len(feature_data)}")
     #one_hour_prediction(feature_data)
 
-    train_x, train_y, test_x, test_y = torch_one_hour_data_split(feature_data)
-    train2_y, test2_y = torch_one_hour_target_split(feature_data, 11)
+    train_x, test_x = gptu.torch_one_hour_data_split(
+        feature_data,
+        11,
+        False
+    )
+    train_y, test_y = gptu.torch_one_hour_target_split(feature_data)
     for i in range(1, 24):
-        tr_y, te_y = torch_one_hour_target_split(feature_data, i)
-        train2_y = torch.vstack([train2_y, tr_y])
-        test2_y = torch.vstack([test2_y, te_y])
+        temp_train_y, temp_test_y = gptu.torch_one_hour_target_split(feature_data, i)
+        train_y = torch.vstack([train_y, temp_train_y])
+        test_y = torch.vstack([test_y, temp_test_y])
 
-    test2_y = torch.squeeze(test2_y)
+    test_y = torch.squeeze(test_y)
     test_x = torch.unsqueeze(test_x, 0)
 
 
     likelihood = gpytorch.likelihoods.GaussianLikelihood()
-    gp_model = gptu.OneDayLinearGP(train_x, train2_y, likelihood)
+    gp_model = gptu.OneDayLinearGP(train_x, train_y, likelihood)
 
     optimizers = [
         torch.optim.Adam(
@@ -139,13 +92,13 @@ def main():
 
     local_pred = [i.mean.numpy() for i in gp_model.pred]
 
-    print(f"\nMAPE: {sp.mape_test(np.array(test2_y), np.array(local_pred)):.3f}")
-    print(f"Mean absolute error: {mae(test2_y, local_pred):.3f}")
-    print(f"r^2 value: {r2_score(test2_y, local_pred):.3f}")
+    print(f"\nMAPE: {sp.mape_test(np.array(test_y), np.array(local_pred)):.3f}")
+    print(f"Mean absolute error: {mae(test_y, local_pred):.3f}")
+    print(f"r^2 value: {r2_score(test_y, local_pred):.3f}")
 
     confi_sigma1 = gptu.confirm_confidence_region(
         local_pred, 
-        test2_y, 
+        test_y, 
         gp_model.upper_sigma, 
         gp_model.lower_sigma
     ) 
@@ -154,7 +107,7 @@ def main():
 
     confi_sigma2 = gptu.confirm_confidence_region(
         local_pred, 
-        test2_y, 
+        test_y, 
         gp_model.upper_two_sigma, 
         gp_model.lower_two_sigma
     )
@@ -165,7 +118,7 @@ def main():
     xlabel = "Time (hours)"
     ylabel = "Load demand (MW)"
 
-    gp_model.plot_pred(test2_y, pred_title, xlabel, ylabel)
+    gp_model.plot_pred(test_y, pred_title, xlabel, ylabel)
 
 
 

c1eede31e39665fabf07c2036f3418a357392708
Francisco Viramontes <fviramontes8@unm.edu>
2022-04-14 09:09:39 -0600

    Update GPyTorchUtils
    Added MultitaskGPModel class to create a multitask Gaussian process,
    different from the other Gaussian process models it takes an extra
    parameter for the number of tasks it will train on.

--- GPyTorchUtils.py
+++ GPyTorchUtils.py
@@ -10,6 +10,27 @@ import gpytorch
 
 import matplotlib.pyplot as plt
 
+class MultitaskGPModel(gpytorch.models.ExactGP):
+    def __init__(self, train_x, train_y, task_number=2):
+        super().__init__(train_x, train_y, likelihood)
+        self.mean_module = gpytorch.means.MultitaskMean(
+            gpytorch.means.ConstantMean(),
+            num_tasks = task_number,
+        )
+        self.covar_module = gpytorch.kernels.MultitaskKernel(
+            gpytorch.kernels.LinearKernel(),
+            num_tasks = task_number,
+            rank=1
+        )
+
+    def forward(self, x):
+        mean_x = self.mean_module
+        covar_x = self.covar_module
+        return gpytorch.distributions.MultitaskMultivariateNormal(
+            mean_x,
+            covar_x
+        )
+
 class OneDayLinearGP():
     def __init__(self, train_x, train_y_mat, likelihood):
         self.train_x = train_x

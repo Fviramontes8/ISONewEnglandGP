4460426a3133d2c3aa661c1ca0fb643b4546855e
Francisco Viramontes <fviramontes8@unm.edu>
2022-07-29 11:07:26 -0600

    Update main
    Generalized input data from an ambiguous ISONE_CA_DEMAND to having
    yearly representations:
        ISONE_CA_DEMAND_2011
        ISONE_CA_DEMAND_2012
        ISONE_CA_DEMAND_2013
        ISONE_CA_DEMAND_2014
        ISONE_CA_DEMAND_2015

--- main.py
+++ main.py
@@ -12,8 +12,7 @@ from sklearn.metrics import mean_absolute_error as mae
 from sklearn.metrics import r2_score
 
 def one_hour_prediction(input_data):
-    train_x, train_y, test_x, test_y = gptu.torch_one_hour_data_split( input_data
-    )
+    train_x, train_y, test_x, test_y = gptu.torch_one_hour_data_split(input_data)
     test_x = test_x.view(1, -1)
 
     likelihood = gpytorch.likelihoods.GaussianLikelihood()
@@ -103,8 +102,12 @@ def one_day_prediction(input_data):
 
     gp_model.plot_pred(test_y, pred_title, xlabel, ylabel)
 
+def multitask_one_day_prediction(input_data):
+    pass
+
 def main():
-    new_england_load_demand_data = np.load("data/ISONE_CA_DEMAND.npy")
+    # The year can change from 2011 to 2015
+    new_england_load_demand_data = np.load("data/ISONE_CA_DEMAND_2011.npy")
     data_cutoff = (24 * 12) #192
     feature_data = np.array(
         new_england_load_demand_data[:data_cutoff],
@@ -115,6 +118,62 @@ def main():
     print(f"Data length: {len(feature_data)}")
     one_hour_prediction(feature_data)
     one_day_prediction(feature_data)
+
+    train_x, test_x = gptu.torch_one_hour_data_split(
+        feature_data,
+        0,
+        False
+    )
+    train_y, test_y = gptu.torch_one_hour_target_split(feature_data)
+    for i in range(1, 24):
+        temp_train_y, temp_test_y = gptu.torch_one_hour_target_split(
+            feature_data,
+            i
+        )
+        train_y = torch.vstack([train_y, temp_train_y])
+        test_y = torch.vstack([test_y, temp_test_y])
+
+    # train_y = torch.transpose(train_y, 0, 1)
+    # train_x = torch.transpose(train_x, 0, 1)
+    test_y = torch.squeeze(test_y)
+    # test_x = torch.unsqueeze(test_x, 0)
+
+    print(f"Train x shape: {train_x.shape}")
+    print(f"Train y shape: {train_y.shape}")
+    print(f"Test x shape: {test_x.shape}")
+    print(f"Test y shape: {test_y.shape}")
+
+    train_rank = torch.linalg.matrix_rank(train_x)
+    training_iter = 50
+    likelihood = gpytorch.likelihoods.MultitaskGaussianLikelihood(
+        num_tasks=train_y.shape[0]
+    )
+
+    print(f"Number of tasks: {train_y.shape[0]}")
+    print(f"Matrix rank of train x: {train_rank}")
+
+    model = gptu.MultitaskGPModel(
+        train_x,
+        train_y,
+        likelihood,
+        train_rank,
+        train_y.shape[0]
+    )
+    opt = torch.optim.Adam(
+        model.parameters(),
+        lr=0.1
+    )
+
+    loss = gptu.TorchTrain(
+        train_x,
+        train_y,
+        model,
+        likelihood,
+        opt,
+        training_iter,
+        True
+    )
+    pu.general_plot(loss, "Training loss", "Time", "Loss")
 
 if __name__ == "__main__":
     main()

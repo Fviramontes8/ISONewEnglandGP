bf5d4a46afe4ca57e13a57e04891657292fed1e1
Francisco Viramontes <fviramontes8@unm.edu>
2024-09-02 20:10:41 -0600

    refactor(main): Simplify main
    Removed esoteric functions before main to begin refactoring. Now data is read from a file, split into training and testing subsets, then the data is graphed, then an auto and crosscorrelation analysis is done

--- main.py
+++ main.py
@@ -1,189 +1,44 @@
 import numpy as np
-import math
-import matplotlib.pyplot as plt
 
-import torch
-import gpytorch
+from isonegp.preprocess import pretrain_checks
+from isonegp.plot import plot_cat_data
+from isonegp.session_generator import create_run_folder
 
-import PlotUtils as pu
-import GPyTorchUtils as gptu
-import SignalProcessor as sp
-from sklearn.metrics import mean_absolute_error as mae
-from sklearn.metrics import r2_score
-
-def one_hour_prediction(input_data):
-    train_x, train_y, test_x, test_y = gptu.torch_one_hour_data_split(input_data)
-    test_x = test_x.view(1, -1)
-
-    likelihood = gpytorch.likelihoods.GaussianLikelihood()
-    gp_model = gptu.LinearGPModel(train_x, train_y, likelihood)
-    optimizer = torch.optim.Adam(
-        [
-            {"params" : gp_model.parameters()},
-        ],
-        lr = 0.1
-    )
-
-    train_loss, train_noise = gptu.TorchTrain(
-        train_x,
-        train_y,
-        gp_model,
-        likelihood,
-        optimizer,
-        50,
-        True
-    )
-    pu.general_plot(train_loss, "Loss over time")
-    pu.general_plot(train_noise, "Noise over time")
-    pred = gptu.TorchTest(test_x, gp_model, likelihood)
-    print(f"{test_y.item()}")
-    print(f"{pred.mean.numpy()}")
-
-def one_day_prediction(input_data):
-    train_x, test_x = gptu.torch_one_hour_data_split(
-        input_data,
-        0,
-        False
-    )
-    train_y, test_y = gptu.torch_one_hour_target_split(input_data)
-    for i in range(1, 24):
-        temp_train_y, temp_test_y = gptu.torch_one_hour_target_split(
-            input_data,
-            i
-        )
-        train_y = torch.vstack([train_y, temp_train_y])
-        test_y = torch.vstack([test_y, temp_test_y])
-
-    test_y = torch.squeeze(test_y)
-    test_x = torch.unsqueeze(test_x, 0)
-
-
-    likelihood = gpytorch.likelihoods.GaussianLikelihood()
-    gp_model = gptu.OneDayLinearGP(train_x, train_y, likelihood)
-
-    optimizers = [
-        torch.optim.Adam(
-            [
-                {"params" : i.parameters(),}
-            ],
-            lr = 0.1
-        )
-        for i in gp_model.models
-    ]
-    gp_model.train(optimizers, 30)
-    gp_model.test(test_x)
-
-    local_pred = [i.mean.numpy() for i in gp_model.pred]
-
-    print(f"\nMAPE: {sp.mape_test(np.array(test_y), np.array(local_pred)):.3f}")
-    print(f"Mean absolute error: {mae(test_y, local_pred):.3f}")
-    print(f"r^2 value: {r2_score(test_y, local_pred):.3f}")
-
-    confi_sigma1 = gptu.confirm_confidence_region(
-        local_pred,
-        test_y,
-        gp_model.upper_sigma,
-        gp_model.lower_sigma
-    ) * 100
-    print(f"{confi_sigma1:.1f}% is contained in a confidence interval", end=' ')
-    print(f"of 1 standard deviation")
-
-    confi_sigma2 = gptu.confirm_confidence_region(
-        local_pred,
-        test_y,
-        gp_model.upper_two_sigma,
-        gp_model.lower_two_sigma
-    ) * 100
-    print(f"{confi_sigma2:.1f}% is contained in a confidence interval", end=' ')
-    print(f"of 2 standard deviations")
-
-    pred_title = "Linear Gaussian process prediction one day ahead"
-    xlabel = "Time (hours)"
-    ylabel = "Load demand (MW)"
-
-    gp_model.plot_pred(test_y, pred_title, xlabel, ylabel)
-
-def multitask_one_day_prediction(input_data):
-    train_x, test_x = gptu.torch_one_hour_data_split(
-        input_data,
-        0,
-        False
-    )
-    train_y, test_y = gptu.torch_one_hour_target_split(input_data)
-    for i in range(1, 24):
-        temp_train_y, temp_test_y = gptu.torch_one_hour_target_split(
-            input_data,
-            i
-        )
-        train_y = torch.vstack([train_y, temp_train_y])
-        test_y = torch.vstack([test_y, temp_test_y])
-
-    # train_y = torch.transpose(train_y, 0, 1)
-    # train_x = torch.transpose(train_x, 0, 1)
-    test_y = torch.squeeze(test_y)
-    test_x = torch.unsqueeze(test_x, 0)
-
-    print(f"Train x shape: {train_x.shape}")
-    print(f"Train y shape: {train_y.shape}")
-    print(f"Test x shape: {test_x.shape}")
-    print(f"Test y shape: {test_y.shape}")
-
-    train_rank = torch.linalg.matrix_rank(train_x)
-    training_iter = 100
-    likelihood = gpytorch.likelihoods.MultitaskGaussianLikelihood(
-        num_tasks=train_y.shape[0]
-    )
-
-    print(f"Number of tasks: {train_y.shape[0]}")
-    print(f"Matrix rank of train x: {train_rank}")
-
-    model = gptu.MultitaskGPModel(
-        train_x,
-        train_y,
-        likelihood,
-        train_rank,
-        train_y.shape[0]
-    )
-    opt = torch.optim.Adam(
-        model.parameters(),
-        lr=0.1
-    )
-
-    loss, noise = gptu.TorchTrain(
-        train_x,
-        train_y,
-        model,
-        likelihood,
-        opt,
-        training_iter,
-        True
-    )
-    #pu.general_plot(loss, "Training loss", "Time", "Loss")
-    #pu.general_plot(noise, "Training noise", "Time", "Noise")
-    pred = gptu.TorchTest(test_x, model, likelihood)
-
-    print(f"Pred length: {pred.mean.numpy().shape}")
-    print(f"Pred:\n{pred.mean.numpy()}")
-    print(f"Actual: {test_y}")
-
-    pred_numpy = pred.mean.numpy()[0,:]
-    pred_error = [math.fabs(i - (j*10000)) for i, j in zip(test_y, pred_numpy)]
-    print(f"Error:\n{pred_error}")
 
 def main():
-    # The year can change from 2011 to 2015
-    new_england_load_demand_data = np.load("data/ISONE_CA_DEMAND_2011.npy")
-    data_cutoff = (24 * 12) #192
-    feature_data = np.array(
-        new_england_load_demand_data[:data_cutoff],
-    )
-    feature_description = "Non-PTF Load Demand"
-    feature_units = "MW"
+    # The year can change from 2011 to 2016
+    new_england_load_demand_data = np.load('data/ISONE_CA_DEMAND_2011.npy')
+    # 12 days
+    training_indices = 24 * 12
+    # 4 days
+    testing_indices = 24 * 4
+    total_indices = training_indices + testing_indices
 
-    print(f"Data length: {len(feature_data)}")
-    #one_hour_prediction(feature_data)
-    #one_day_prediction(feature_data)
-    multitask_one_day_prediction(feature_data)
-    
-if __name__ == "__main__":
+    demand_training_data = np.array(
+        new_england_load_demand_data[:training_indices],
+    )
+    demand_testing_data = np.array(
+        new_england_load_demand_data[training_indices:total_indices]
+    )
+    demand_description = 'Non-PTF Load Demand'
+    demand_units = 'MegaWatts'
+
+    current_run_folder = create_run_folder()
+    plot_cat_data(
+        [demand_training_data, demand_testing_data],
+        ['c', 'm'],
+        ['Training data', 'Testing data'],
+        demand_description,
+        'Hours',
+        demand_units,
+        True,
+        current_run_folder,
+        'training_testing_data_fullview',
+    )
+    pretrain_checks(
+        demand_training_data, demand_testing_data, current_run_folder
+    )
+
+
+if __name__ == '__main__':
     main()

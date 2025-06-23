6f76e709928a487163c423affd83f194cdaf1d64
Francisco Viramontes <fviramontes8@unm.edu>
2025-06-22 20:23:54 -0600

    feat(session_generator): Update session_generator.py
    Replaces all mentions of sessions with runs

--- isonegp/session_generator.py
+++ isonegp/session_generator.py
@@ -2,16 +2,16 @@ from os import getcwd, makedirs, listdir
 from os.path import exists, join
 
 
-def validate_session_folder() -> None:
+def validate_run_folder() -> None:
     """
-    Creates a folder called 'sessions' where the main program runs
+    Creates a folder called 'runs' where the main program runs
 
     Returns
     -------
     None
     """
-    if (not exists(join(getcwd(), "sessions"))):
-        makedirs(join(getcwd(), "sessions"), exist_ok=True)
+    if not exists(join(getcwd(), "runs")):
+        makedirs(join(getcwd(), "runs"), exist_ok=True)
 
 
 def create_run_folder() -> str:
@@ -24,15 +24,15 @@ def create_run_folder() -> str:
     max_run_str: str
     String that contains the folder name to store figures and logs
     """
-    validate_session_folder()
-    session_folders: list[str] = listdir(join(getcwd(), "sessions"))
+    validate_run_folder()
+    session_folders: list[str] = listdir(join(getcwd(), "runs"))
     max_run: int = -1
     for folder in session_folders:
-        if folder.startswith("run"):
-            current_run = int(folder[3:])
+        if folder.startswith("train"):
+            current_run = int(folder[5:])
             if current_run > max_run:
                 max_run = current_run
     max_run += 1
-    makedirs(join(getcwd(), f"sessions/run{max_run}/figs"), exist_ok=True)
-    max_run_str: str = f"run{max_run}"
+    makedirs(join(getcwd(), f"runs/train{max_run}/figs"), exist_ok=True)
+    max_run_str: str = f"train{max_run}"
     return max_run_str
